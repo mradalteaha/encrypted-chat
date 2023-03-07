2f2be1f0171979c61534d5805c52522ac7d0a862
Morad Teaha <mrad1999@gmail.com>
2023-03-07 11:44:01 +0200

    updating Profile

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -9,34 +9,34 @@ export default function ItemList({type,description,user,style,time ,room ,image,
     const navigation = useNavigation()
     const {theme:{colors}} = useContext(GlobalContext)
 
-   console.log("printing user")
-   console.log(user);
+   
+  // console.log(user);
 
   return (
     <TouchableOpacity style={{height:80,borderRadius:30, ...style}} onPress={()=>navigation.navigate("ChatScreen",{user,room,image})}>
-      <Grid style={{maxHeight:80}} >
-      <Col style={{width:80,alignItems:'center',justifyContent:'center'}}>
-        <Avatar user={user} size={type ==='contacts' ? 40 : 60}/>
-      </Col>
-      <Col style={{marginLeft:10}}>
-        <Row style={{alignItems:'center'}}>
-            <Col>
-              <Text style={{fontWeight:'bold',fontSize:16,color:colors.text}}>
-                {user.displayName}
-              </Text>
-            </Col>
-            
-              {time && (<Col style={{alignItems:"flex-end", }}>
-                <Text style={{color:colors.secondaryText}}>{new Date(time.seconds *1000).toLocaleDateString()}</Text>
-              </Col>)}
-        </Row>
-        {description &&(
-          <Row style={{marginTop:-5}}>
-          <Text style={{color:colors.secondaryText}}>{description}</Text>
-          </Row>
-        )}
-      </Col>
-      </Grid>  
+        <Grid style={{maxHeight:80}} >
+          <Col style={{width:80,alignItems:'center',justifyContent:'center'}}>
+            <Avatar user={user} size={type ==='contacts' ? 40 : 60}/>
+          </Col>
+          <Col style={{marginLeft:10}}>
+            <Row style={{alignItems:'center'}}>
+                <Col>
+                  <Text style={{fontWeight:'bold',fontSize:16,color:colors.text}}>
+                    {user.displayName}
+                  </Text>
+                </Col>
+                
+                  {time && (<Col style={{alignItems:"flex-end", }}>
+                    <Text style={{color:colors.secondaryText}}>{new Date(time.seconds *1000).toLocaleDateString()}</Text>
+                  </Col>)}
+            </Row>
+            {description &&(
+              <Row style={{marginTop:-5}}>
+              <Text style={{color:colors.secondaryText}}>{description}</Text>
+              </Row>
+            )}
+          </Col>
+        </Grid>  
     </TouchableOpacity>
   )
 }
